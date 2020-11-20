d62a8528d8290f542a8ab66f6ee5791c3634e30a
Ricky Martinez <martinez.ricky95@gmail.com>
2020-11-20 14:31:50 -0500

    Integrated the SPAHack and Created new pages

---
+++ html/js/SPAHack.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var location_name = location.href.split("/")[3];
+    
+    // Defaut load
+    if(location_name == "index.html" || location_name == ""){
+        $("#content").load("html/pages/home.html");
+    }
+    
+    // Navbar Links
+        $("a.nav-link").unbind("click").click(function(){            
+            var sectionUrl = $(this).attr("data-url");
+            console.log(sectionUrl);
+            $("#content").load(sectionUrl, function(){
+                console.log("Loaded " + sectionUrl);
+            })
+        });
+});
