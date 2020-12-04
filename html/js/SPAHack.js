5e9a43e26a39d3cf576ec0ec5d1e6d311e2388cf
Ricky Martinez <martinez.ricky95@gmail.com>
2020-12-03 23:14:08 -0500

    Edited some paths

--- html/js/SPAHack.js
+++ html/js/SPAHack.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
     
     // Defaut load
     if(location_name == "index.html" || location_name == ""){
-        $("#content").load("html/pages/home.html");
+        $("#content").load("../pages/home.html");
     }
     
     // Navbar Links
-        $("a.nav-link").unbind("click").click(function(){            
-            var sectionUrl = $(this).attr("data-url");
-            console.log(sectionUrl);
-            $("#content").load(sectionUrl, function(){
-                console.log("Loaded " + sectionUrl);
-            })
-        });
+    $("a.nav-link").unbind("click").click(function(){            
+        var sectionUrl = $(this).attr("data-url");
+        console.log(sectionUrl);
+        $("#content").load(sectionUrl, function(){
+            console.log("Loaded " + sectionUrl);
+        })
+    });
 });
